f9ab2ee2dc836fe582b50acbddf2461ac5485a2f
Gustavo Dambros <gustavodambros.19@gmail.com>
2025-11-30 16:29:44 -0300

    atualizacoes tailwind

--- locare_tailwind/tailwind-preset.js
+++ locare_tailwind/tailwind-preset.js
@@ -4,10 +4,12 @@ module.exports = {
     extend: {
       colors: {
         bg_primary:'#242426',
-        bg_secondary:'#444447',
+        bg_secondary:'i
+        
         acent_color:'#28b55e',
         diactivate_color:'#053B20',
-        text_color:'#F3ECF5'
+        text_color:'#F3ECF5',
+        text_color_activate:'#BEB8BF',
 
       },
     },
