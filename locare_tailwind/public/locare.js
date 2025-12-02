9c6ddcfb256e49a2bf95847950b4c4f1a2d6c7fd
Gustavo Dambros <gustavodambros.19@gmail.com>
2025-12-02 20:16:46 -0300

    .

--- locare_tailwind/public/locare.js
+++ locare_tailwind/public/locare.js
@@ -1,14 +1,22 @@
-
-function mudarMenu(){
    
 const menu=document.getElementById("menu_icon");
+const sidebar=document.getElementById("sidebar")
+
+function mudarMenu(){
+
+sidebar.classList.toggle("translate-x-0");
+sidebar.classList.toggle("translate-x-full");
 menu.src='./Img/menu_icon.svg';
 
 }
 
-function mostrar(){
+document.addEventListener('click', (e) =>    {
+   const clicouFora = !sidebar.contains(e.target);
+    const clicouNoBotao = menu.contains(e.target);
 
-const name=document.getElementById("name").value
-const boasVindas=document.getElementById("boasVindas").textContent=name
+    if (clicouFora && !clicouNoBotao) {
+        sidebar.classList.remove("translate-x-0");
+        sidebar.classList.add("translate-x-full");
 
-};
+}})
+
